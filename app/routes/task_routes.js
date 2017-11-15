14c8dcbddbab119db067e08263335b635a307395
Kun Ma <makun211@gmail.com>
2017-11-15 02:21:48 -0800

    finished app

--- app/routes/task_routes.js
+++ app/routes/task_routes.js
@@ -1,7 +1,54 @@
+let ObjectID = require('mongodb').ObjectID
+
+
 module.exports = (app, db) => {
-  app.post('/task', (req, res) => {
-    // You'll create your note here.
-    console.log(req.body)
-    res.send('Hello')
+  app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id
+    const details = { '_id': new ObjectID(id) }
+    db.collection('tasks').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'})
+      } else {
+        res.send(item)
+      }
+    })
+  })
+
+  app.post('/tasks', (req, res) => {
+    // You'll create your task here.
+    const task = { text: req.body.body, title: req.body.title }
+    db.collection('tasks').insert(task, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' })
+      } else {
+        console.log(result)
+        res.send(result.ops[0])
+      }
+    })
+  })
+
+  app.delete('/tasks/:id', (req, res) => {
+    const id = req.params.id
+    const details = { '_id': new ObjectID(id) }
+    db.collection('tasks').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'})
+      } else {
+        res.send('Task ' + id + ' deleted!')
+      }
+    })
+  })
+
+  app.put('/tasks/:id', (req, res) => {
+    const id = req.params.id
+    const details = { '_id': new ObjectID(id) }
+    const task = { text: req.body.body, title: req.body.title }
+    db.collection('tasks').update(details, task, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'})
+      } else {
+          res.send(task)
+      }
+    })
   })
 }
--- server.js
+++ server.js
@@ -3,11 +3,15 @@
 const express = require('express'),
   MongoClient = require('mongodb').MongoClient,
   bodyParser = require('body-parser'),
+  db = require('./config/db'),
   app = express(),
   port = 8000
 app.use(bodyParser.urlencoded({ extended: true }))
-require('./app/routes')(app, {})
 
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database)
   app.listen(port, () => {
     console.log('We are live on ' + port)
   })
+})
