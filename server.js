0c91833b840050d38747d6cf61235415dbcb80ff
Kun Ma <makun211@gmail.com>
2017-11-15 16:41:37 -0800

    Revised server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express'),
   app = express(),
   port = 8080
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
